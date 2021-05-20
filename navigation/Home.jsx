53c1c1bd747b1dc1c05410e6dad80e872ca86242
dany gbelidji <dany.gbelidji@live.fr>
2021-05-20 15:44:35 +0200

    💄 MOD - style app button

--- navigation/Home.jsx
+++ navigation/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Text, SafeAreaView, FlatList, View, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, FlatList, View, StyleSheet, ActivityIndicator } from 'react-native';
+// import { Icon } from 'react-native-elements';
 
 import AppButton from '../components/AppButton'
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 
 const programmes = [
     {
@@ -27,43 +29,73 @@ const programmes = [
         poster_image: "https://i.ibb.co/D57J8TW/MAIN-AVANCE.png",
     },
 ];
+// const programmes = [];
 const colors = ["#BA1D35", "#F2D815", "#00C4C1"]
+const icons = ["home"]
+
+// const getIcons = (level) => {
+//     let icon;
+//     switch (level) {
+//         case 1:
+//             icon = "accessibility-outline";
+//             break;
+//         case 2:
+//             icon = "fitness-outline";
+//             break;
+//         case 3:
+//             icon = "flash-outline";
+//             break;
+//         default:
+//             break;
+//     }
+//     return icon
+// }
+// <Icon
+// name={getIcons(item.level)}
+// type='ionicon'
+// />
+
 export default function Home(props) {
     // console.log(`Home -> props`, props)
     const { route, navigation } = props;
     return (
         <SafeAreaView style={styles.main_container}>
-            <FlatList
-                showsVerticalScrollIndicator={false} // Enlève les barres de scroll sur l'axe y
-                showsHorizontalScrollIndicator={false} // Enlève les barres de scroll sur l'axe x
-                data={programmes} // => tableau avec les données à afficher /!\
-                keyExtractor={(item) => item._id.toString()} // => toujours envoyer la clé unique /!\ en caractère => permet de définir un id unique à chaque élément
-                renderItem={({ item, index }) => ( //=> /!\ bien spécifier les accolades à l'intérieur des paramètres à envoyer -> c'est un objet
-                    <>
-                        <AppButton
+            {programmes.length > 0 // Si la taille de mon tableau "programme" est >0 alors j'affiche mes éléments, sinon j'affiche mon élément de chargement
+                ? (<FlatList
+                    showsVerticalScrollIndicator={false} // Enlève les barres de scroll sur l'axe y
+                    showsHorizontalScrollIndicator={false} // Enlève les barres de scroll sur l'axe x
+                    data={programmes} // => tableau avec les données à afficher /!\
+                    keyExtractor={(item) => item._id.toString()} // => toujours envoyer la clé unique /!\ en caractère => permet de définir un id unique à chaque élément
+                    renderItem={({ item, index }) => ( //=> /!\ bien spécifier les accolades à l'intérieur des paramètres à envoyer -> c'est un objet
+                        <>
+                            <AppButton
 
-                            onPress={() => {
-                                //On définit vers quel screen on veux aller, grace à la props name définit dans App
-                                navigation.navigate("Programme", {
-                                    title: item.name
-                                    , level: item.level
-                                    , id: item._id
-                                })
-                            }}
-                            title={item.name}
-                            image={item.poster_image}
-                            color={colors[index]}
-                            textPosition={index % 2 ? "left" : "right"}
+                                onPress={() => {
+                                    //On définit vers quel screen on veux aller, grace à la props name définit dans App
+                                    navigation.navigate("Programme", {
+                                        title: item.name
+                                        , level: item.level
+                                        , id: item._id
+                                    })
+                                }}
+                                title={item.name}
+                                image={item.poster_image}
+                                color={colors[index]}
+                                textPosition={index % 2 ? "left" : "right"}
 
-                        />
-                        <View>
-                            <MaterialCommunityIcons name="home" size={20} />
-                            <Text>{item.description}</Text>
-                        </View>
-                    </>
+                            />
+                            <View style={styles.description}>
+                                <MaterialCommunityIcons name="home" size={20} />
 
-                )}
-            />
+                                <Text style={styles.textDescription}>{item.description}</Text>
+                            </View>
+                        </>
+
+                    )}
+                />)
+                : (<ActivityIndicator style={styles.loader} size="large" color="#00C4C1" />)
+            }
+
         </SafeAreaView>
     )
 }
@@ -71,9 +103,26 @@ export default function Home(props) {
 const styles = StyleSheet.create({
     main_container: {
         flex: 1 // prends la largeur total de la colonne => flexDirection par défaut en colonne
-        , marginTop: 20 // unité densité de pixel
+        , marginTop: 10 // unité densité de pixel
         , padding: 10
+
         // , marginHorizontal: 10
     },
+    loader: {
+        flex: 1
+    },
+    description: {
+        backgroundColor: '#C7CEDE'
+        , flexDirection: 'row'
+        , flex: 1
+        , marginHorizontal: 10
+        , paddingVertical: 5
+        , paddingLeft: 5
+        , marginBottom: 15
+    },
+    textDescription: {
+        marginLeft: 5
+        , fontWeight: "bold"
+    }
 
 })
