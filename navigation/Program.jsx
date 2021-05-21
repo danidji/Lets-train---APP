40b1be728d495dd014e8dfa75eaf8627c9ebd587
dany gbelidji <dany.gbelidji@live.fr>
2021-05-21 16:46:40 +0200

    🚧 WIP - Display sub programs on program screen

--- navigation/Program.jsx
+++ navigation/Program.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, SafeAreaView, FlatList, View, StyleSheet, ActivityIndicator } from 'react-native';
 
-export default function Program(props) {
+//Import redux
+import { connect, useDispatch, useSelector } from "react-redux"
+import { getSubPrograms } from '../store/actions';
+
+//init variable de connection redux
+const mapStateToProps = (state) => {
+    return state;
+}
+
+
+
+
+
+function Program(props) {
+    const { route } = props
+    // console.log(`Program -> props`, route.params.id)
+
+    const dispatch = useDispatch();
+    const subPrograms = useSelector(
+        ({ subprogramsReducer }) => subprogramsReducer.subPrograms
+    );
+
+    useEffect(() => {
+        dispatch(getSubPrograms(route.params.id));
+    }, [dispatch])
+
+
+    // console.log(`Program -> subPrograms`, subPrograms)
+
     return (
         <SafeAreaView>
-            <Text>HOME</Text>
+
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                data={subPrograms}
+                keyExtractor={(item) => item._id.toString()}
+                renderItem={({ item, index }) => (
+                    <>
+                        <Text>{item.title}</Text>
+                    </>
+                )}
+            />
+
+
+
+
         </SafeAreaView>
     )
 }
+
+
+export default connect(mapStateToProps)(Program);
